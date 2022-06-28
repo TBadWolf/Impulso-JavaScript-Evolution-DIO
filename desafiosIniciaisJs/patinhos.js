e41ec607ca821880a76ac6820d7809434fca0205
TBadWolf <barbara.rce@hotmail.com>
2022-06-28 18:59:26 -0300

    Desafios Iniciais - Impulso JavaScript Evolution - DIO
    Desafios Iniciais - Impulso JavaScript Evolution - DIO/ 2022

---
+++ desafiosIniciaisJs/patinhos.js
@@ -0,0 +1,9 @@
+let n = parseInt(gets());
+        
+while(n != -1) { 
+        if ( n <= 0 ) 
+            print( 0 ); 
+        else 
+            print( n - 1 );        
+        n = parseInt(gets()); 
+    }
